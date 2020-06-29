c2fcdae3b0cbe1581d1c8b0d29020848fe0e63e0
Vladimir Romanovich <ibnteo@gmail.com>
2020-06-29 15:21:04 +0300

    syms

--- Arduino/catboard4.ino
+++ Arduino/catboard4.ino
@@ -1,7 +1,7 @@
 /*
    Keyboard CatBoard-4
-   Version: 0.1
-   Date: 2020-06-26
+   Version: 0.2
+   Date: 2020-06-29
    Author: Vladimir Romanovich <ibnteo@gmail.com>
    License: MIT
    Controller: ProMicro (Arduino Leonardo)
@@ -112,8 +112,9 @@ byte oskey = LINUX;
 #define IS_MODS(code) (code >= KEY_LEFT_CTRL && code <= KEY_RIGHT_GUI)
 #define IS_MOD3(code) (code >= KEY_LEFT_CTRL && code <= KEY_RIGHT_GUI && code != KEY_LEFT_SHIFT && code != KEY_RIGHT_SHIFT)
 
-#define KEY_MOUSE 246
-#define KEY_X4    247
+#define KEY_MOUSE 245
+#define KEY_X4    246
+#define KEY_MACRO 247
 #define KEY_USYM  248
 #define KEY_SYM   249
 #define KEY_LAY1  250
@@ -126,6 +127,40 @@ byte oskey = LINUX;
 #define LAY_FN(lay) (4 + lay - KEY_FN)
 #define IS_LAY(code) (code >= KEY_LAY1 && code <= KEY_LAY2)
 #define LAY(lay) (lay - KEY_LAY1)
+
+byte layer = 0;
+byte mlast  = 0;
+byte mlayer = 0;
+byte pressed[KEYS] = {};
+byte mods = 0;
+
+void layout(byte l) {
+  if (layer != l) {
+    Keyboard.releaseAll();
+    if (oskey == MACOS) {
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press(' ');
+    } else if (oskey == WINDOWS) {
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(KEY_LEFT_CTRL);
+    } else if (oskey == LINUX) {
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(KEY_LEFT_ALT);
+    }
+    Keyboard.releaseAll();
+    mrecover();
+  }
+  layer = l;
+  digitalWrite(LED_LAYER, layer == 0 ? HIGH : LOW); // LOW = light
+}
+
+void mrecover() {
+  for (byte i = 0; i < KEYS; i++) {
+    if (pressed[i] >= KEY_MODS && pressed[i] <= KEY_RIGHT_GUI) {
+      Keyboard.press(pressed[i]);
+    }
+  }
+}
 
 const char layers[][KEYS] PROGMEM = {
   { // Layer mods
@@ -162,7 +197,7 @@ const char layers[][KEYS] PROGMEM = {
     KEY_X4, '1', '2', '3', '4', '5',
     KEY_TAB, '6', '7', '8', '9', '0',
     KEY_LAY2, '\\', '=', '-', '`',
-    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_FN3,
+    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_LEFT_SHIFT,
     KEY_MOUSE, KEY_HOME, KEY_UP_ARROW, KEY_END, KEY_PAGE_UP, KEY_ESC,
     KEY_RETURN, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_PAGE_DOWN, KEY_FN2,
     KEY_BACKSPACE, KEY_DELETE, KEY_INSERT, KEY_APPLICATION, KEY_LAY1,
@@ -170,9 +205,9 @@ const char layers[][KEYS] PROGMEM = {
   },
   { // Fn
     KEY_X4, '1', '2', '3', '4', '5',
-    KEY_LEFT_SHIFT, '6', '7', '8', '9', '0',
+    KEY_FN4, '6', '7', '8', '9', '0',
     KEY_LAY2, '/', '+', '-', '.',
-    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_FN3,
+    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_LEFT_SHIFT,
     KEY_MOUSE, KEY_HOME, KEY_UP_ARROW, KEY_END, KEY_PAGE_UP, KEY_ESC,
     KEY_RETURN, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_PAGE_DOWN, KEY_FN2,
     KEY_BACKSPACE, KEY_DELETE, KEY_INSERT, KEY_APPLICATION, KEY_LAY1,
@@ -192,20 +227,20 @@ const char layers[][KEYS] PROGMEM = {
     KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
     KEY_FN4, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
     KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
-    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_FN3,
-    KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
-    KEY_RETURN, KEY_LEFT_ARROW, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
-    KEY_BACKSPACE, ' ', KEY_SYM, KEY_SYM, KEY_SYM,
+    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_LEFT_SHIFT,
+    KEY_MOUSE, KEY_HOME, KEY_UP_ARROW, KEY_END, KEY_PAGE_UP, KEY_ESC,
+    KEY_RETURN, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_PAGE_DOWN, KEY_FN2,
+    KEY_BACKSPACE, KEY_DELETE, KEY_INSERT, KEY_APPLICATION, KEY_LAY2,
     ' ', KEY_FN, KEY_RIGHT_ALT
   },
   { // Fn4
-    KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
-    KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
-    KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
-    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_FN3,
-    KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
+    KEY_MACRO, KEY_MACRO, KEY_MACRO, KEY_MACRO, KEY_MACRO, KEY_MACRO,
     KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
     KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM, KEY_USYM,
+    KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_LEFT_SHIFT,
+    KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
+    KEY_RETURN, KEY_LEFT_ARROW, KEY_SYM, KEY_SYM, KEY_SYM, KEY_SYM,
+    KEY_BACKSPACE, KEY_DELETE, KEY_SYM, KEY_SYM, KEY_SYM,
     ' ', KEY_FN, KEY_RIGHT_ALT
   }
 };
@@ -220,40 +255,6 @@ const char syms[KEYS][2] PROGMEM = {
   {0, 0}, {0, 0}, {'<', 0}, {'>', 0}, {0, 0},
   {' ', ' '}, {0, 0}, {0, 0}
 };
-
-byte layer = 0;
-byte mlast  = 0;
-byte mlayer = 0;
-byte pressed[KEYS] = {};
-byte mods = 0;
-
-void layout(byte l) {
-  if (layer != l) {
-    Keyboard.releaseAll();
-    if (oskey == MACOS) {
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press(' ');
-    } else if (oskey == WINDOWS) {
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-    } else if (oskey == LINUX) {
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_ALT);
-    }
-    Keyboard.releaseAll();
-    mrecover();
-  }
-  layer = l;
-  digitalWrite(LED_LAYER, layer == 0 ? HIGH : LOW); // LOW = light
-}
-
-void mrecover() {
-  for (byte i = 0; i < KEYS; i++) {
-    if (pressed[i] >= KEY_MODS && pressed[i] <= KEY_RIGHT_GUI) {
-      Keyboard.press(pressed[i]);
-    }
-  }
-}
 
 void symbols(byte k) {
   byte l = layer;
@@ -271,19 +272,52 @@ void symbols(byte k) {
 }
 
 String macros(byte k) {
-  if (k == 21 && layout == 0) return "tion";
+  if (k == 1 && layer == 0) return "Youtube";
+  if (k == 1 && layer == 1) return "djpvj;yj"; // возможно
+
+  if (k == 2 && layer == 0) return "length";
+  if (k == 2 && layer == 1) return "wbjyy"; // ционн
+  
+  if (k == 3 && layer == 0) return "return";
+  if (k == 3 && layer == 1) return "cnjhjy"; // сторон
+  
+  if (k == 4 && layer == 0) return "foreach";
+  if (k == 4 && layer == 1) return "ghj,ktv"; // проблем
+  
+  if (k == 5 && layer == 0) return "function";
+  if (k == 5 && layer == 1) return "xtkjdt"; // челове
+  
+  if (k == 6 && layer == 0) return "right";
+  if (k == 6 && layer == 1) return "jnyjityb"; // отношени
+  
   return "";
 }
 
 String usyms(byte k) {
-  if (k == 35 && os <= LINUX) return os == LINUX_COMPOSE ? "<<" : "00ab";
-  else if (k == 35 && os == WINDOWS) return "0171";
+  if (k == 8 && os <= LINUX) return os == LINUX_COMPOSE ? "<<" : "0301";
+  else if (k == 8 && os == WINDOWS) return "0301";
 
-  else if (k == 36 && os <= LINUX) return os == LINUX_COMPOSE ? ">>" : "00bb";
-  else if (k == 36 && os == WINDOWS) return "0187";
-  
+  else if (k == 9 && os <= LINUX) return os == LINUX_COMPOSE ? "<<" : "00ab";
+  else if (k == 9 && os == WINDOWS) return "0171";
+
+  else if (k == 10 && os <= LINUX) return os == LINUX_COMPOSE ? ">>" : "00bb";
+  else if (k == 10 && os == WINDOWS) return "0187";
+
+  else if (k == 11 && os <= LINUX) return os == LINUX_COMPOSE ? ">>" : "00a0";
+  else if (k == 11 && os == WINDOWS) return "0160";
+
+  else if (k == 12 && os <= LINUX) return os == LINUX_COMPOSE ? ">>" : "00b0";
+  else if (k == 12 && os == WINDOWS) return "0176";
+
+  else if (k == 15 && os <= LINUX) return os == LINUX_COMPOSE ? "x" : "00d7";
+  else if (k == 15 && os == WINDOWS) return "0215";
+
+  else if (k == 16 && os <= LINUX) return os == LINUX_COMPOSE ? "--" : "2013";
+  else if (k == 16 && os == WINDOWS) return "2013";
+
   else if (k == 17 && os <= LINUX) return os == LINUX_COMPOSE ? "---" : "2014";
-  //else if (k == 17 && os == WINDOWS) return "0187";
+  else if (k == 17 && os == WINDOWS) return "2014";
+
   return "";
 }
 
@@ -329,11 +363,24 @@ void press(byte k) {
   byte code = pgm_read_byte(&layers[l][k - 1]);
 
   bool is_mods = false;
+  bool is_shift = false;
+  bool is_fn2 = false;
   for (byte i = 0; i < KEYS; i++) {
     if (IS_MOD3(pressed[i])) {
       is_mods = true;
-      break;
     }
+    if (pressed[i] == KEY_LEFT_SHIFT) {
+      is_shift = true;
+    }
+    if (pressed[i] == KEY_FN2) {
+      is_fn2 = true;
+    }
+  }
+
+  if (is_shift && mlayer == KEY_FN && k <= 20) { // Fn + Shift
+    Keyboard.release(KEY_LEFT_SHIFT);
+    l = LAY_FN(mlayer + 2);
+    code = pgm_read_byte(&layers[l][k - 1]);
   }
   
   if (IS_FN(code)) { // Fn
@@ -349,10 +396,11 @@ void press(byte k) {
         }
       }
     }
-    if (code == KEY_FN2) {
-      code = 0;
-    }
   
+  } else if (! is_fn2 && mlayer == KEY_FN2 && code >= KEY_CAPS_LOCK && code <= KEY_F12) { // Func once
+    mlayer = KEY_FN;
+    Keyboard.press(code);
+    
   } else if (code == KEY_LEFT_ALT || code == KEY_RIGHT_ALT) { // Alt + AltGr = Win
     for (byte i = 0; i < KEYS; i++) {
       if (pressed[i] == KEY_LEFT_ALT || pressed[i] == KEY_RIGHT_ALT) {
@@ -376,18 +424,23 @@ void press(byte k) {
   } else if (code == KEY_USYM) { // Unicode Symbol
     unicode(usyms(k), true);
 
+  } else if (code == KEY_MACRO) { // Macros
+    Keyboard.print(macros(k));
+
   } else if (mlayer == KEY_FN && is_mods) { // Fn mods
     l = LAY_FN(KEY_FN - 1);
     code = pgm_read_byte(&layers[l][k - 1]);
     Keyboard.press(code);
 
   } else if (is_mods) { // Layer mods
-    l = layer;
-    code = pgm_read_byte(&layers[l][k - 1]);
+    code = pgm_read_byte(&layers[0][k - 1]);
     Keyboard.press(code);
   
   } else {
     Keyboard.press(code);
+  }
+  if (is_shift) {
+    Keyboard.press(KEY_LEFT_SHIFT);
   }
   pressed[k] = code;
   mlast = code;
@@ -400,7 +453,7 @@ void release(byte k) {
   }
   byte code = pressed[k];
   pressed[k] = 0;
-  if (IS_FN(code)) { // Fn
+  if (IS_FN(code) && code != KEY_FN2) { // Fn
     mlayer = 0;
     for (byte i = 0; i < KEYS; i++) {
       if (IS_FN(pressed[i])) {
@@ -414,7 +467,7 @@ void release(byte k) {
       }
     }
   }
-  if (code == mlast && (IS_MODS(code) || IS_FN(code))) { // Mods click
+  if (code == mlast && (IS_MODS(code) || IS_FN(code)) && code != KEY_FN2) { // Mods click
     Keyboard.release(code);
     code = pgm_read_byte(&layers[0][k - 1]);
     if (! IS_MODS(code) && ! IS_FN(code)) {
